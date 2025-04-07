4ea5cb803de63fbd362d97eb4cfb7e41d6d15206
taylor1987 <richard.taylor@mpl.mpg.de>
2025-04-07 19:39:22 +0200

    ...

--- GUI/TEST/SerialProtocol.h
+++ GUI/TEST/SerialProtocol.h
@@ -3,21 +3,27 @@
 
 #include <stdint.h>  // for STM32 compatibility
 
-// Protocol markers
+#ifdef __cplusplus
+// C++ version with constexpr
 constexpr uint8_t START_MARKER = '<';
 constexpr uint8_t END_MARKER = '>';
-
-// Message size (including start and end markers)
 constexpr int MESSAGE_SIZE = 7;
-
-// Serial settings
 constexpr uint32_t SERIAL_BAUD_RATE = 115200;
 constexpr uint8_t SERIAL_DATA_BITS = 8;
 constexpr uint8_t SERIAL_STOP_BITS = 1;
-constexpr uint8_t SERIAL_PARITY_NONE = 0; // define values to share meaning
-
-// Buffer size for incoming data (desktop only)
+constexpr uint8_t SERIAL_PARITY_NONE = 0;
 constexpr int HOLDING_BUFFER_SIZE = 1000;
+#else
+// C version with #define
+#define START_MARKER '<'
+#define END_MARKER '>'
+#define MESSAGE_SIZE 7
+#define SERIAL_BAUD_RATE 115200
+#define SERIAL_DATA_BITS 8
+#define SERIAL_STOP_BITS 1
+#define SERIAL_PARITY_NONE 0
+#define HOLDING_BUFFER_SIZE 1000
+#endif
 
 // Byte layout:
 // [0] = START_MARKER
@@ -26,7 +32,11 @@ constexpr int HOLDING_BUFFER_SIZE = 1000;
 // [6] = END_MARKER
 
 // Optional helper to calculate checksum
+#ifdef __cplusplus
 inline uint8_t calculateChecksum(const uint8_t *data, int length) {
+#else
+static inline uint8_t calculateChecksum(const uint8_t *data, int length) {
+#endif
     uint8_t sum = 0;
     for (int i = 0; i < length; ++i) {
         sum += data[i];
